beb76bca1903a4f9e33594297eb3c7a3cef0d8b2
sumit1595 <sumitlodwal@gmail.com>
2020-01-10 15:13:03 -0500

    s

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.text.DateFormat;
+import java.util.Date;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    @Size(min = 4)
+    private String content;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date posteddate;
+    @NotNull
+    @Size(min = 5)
+    private String sentby;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPosteddate() {
+        return posteddate;
+    }
+
+    public void setPosteddate(Date posteddate) {
+        this.posteddate = posteddate;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
